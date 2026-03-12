41c4ab816ae7c39ec36405c5fdac1a7d8490ee08
Vincent <72661240+vncntwbr@users.noreply.github.com>
2026-03-12 14:29:04 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,11 @@
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('pwa-cache').then(cache => cache.add('index.html'))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
